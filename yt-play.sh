0f722e92819a27aa1afb7b069c8784169a223353
XUser <erika75ungruhe@gmail.com>
2026-01-13 16:59:08 +0100

    Update yt-play.sh

--- yt-play.sh
+++ yt-play.sh
@@ -8,4 +8,4 @@ export KITTY_DISABLE_WAYLAND=1
 export GDK_BACKEND=x11
 export PYTHONWARNINGS="ignore"
 
-kitty --start-as=fullscreen sh -c "mpv --no-config --vo=kitty --ytdl-format='bestvideo[height<=240]+bestaudio/best' --video-sync=audio --really-quiet '$url' 2>/dev/null" 2>/dev/null
+kitty --start-as=fullscreen sh -c "mpv --no-config --vo=tct --ytdl-format='bestvideo[height<=240]+bestaudio/best' --video-sync=audio --really-quiet '$url' 2>/dev/null" 2>/dev/null
