5131d8debef2e5c4d8a52da9d3275fee41a9cff8
XUser <erika75ungruhe@gmail.com>
2026-01-13 17:00:24 +0100

    Create yt-player-Q.sh

---
+++ yt-player-Q.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+url=$1
+if [ -z "$url" ]; then
+    read -p "YouTube URL: " url
+fi
+
+export KITTY_DISABLE_WAYLAND=1
+export GDK_BACKEND=x11
+export PYTHONWARNINGS="ignore"
+
+kitty --start-as=fullscreen sh -c "mpv --no-config --vo=kitty --ytdl-format='bestvideo[height<=240]+bestaudio/best' --video-sync=audio --really-quiet '$url' 2>/dev/null" 2>/dev/null
